f8084b51dff1b19025dec8608a68cf648a833b0b
yuj-2 <tree9030@naver.com>
2026-02-19 16:41:49 +0900

    0219

---
+++ culti/src/main/java/com/culti/HomeController.java
@@ -0,0 +1,22 @@
+package com.culti;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class HomeController {
+
+	@GetMapping("/home")
+	public void home() {
+		
+	}
+	
+	
+}
---
+++ culti/src/test/java/com/culti/CultiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
