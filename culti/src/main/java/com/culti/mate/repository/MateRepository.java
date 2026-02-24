42583b50a348fd899aeb944ca0ea0dd2afe6b550
yuj-2 <tree9030@naver.com>
2026-02-24 16:38:23 +0900

    푸터 추가, 동행 수정

--- culti/src/main/java/com/culti/mate/repository/MateRepository.java
+++ culti/src/main/java/com/culti/mate/repository/MateRepository.java
@@ -1,6 +1,8 @@
-package com.culti.mate;
+package com.culti.mate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.culti.mate.entity.MatePost;
 
 public interface MateRepository extends JpaRepository<MatePost, Long> {
 
