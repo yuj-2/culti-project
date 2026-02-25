9c297b280b9588d555a20e89cf5f33cec8e84c59
yuj-2 <tree9030@naver.com>
2026-02-25 17:42:56 +0900

    풀 받기 전 커밋

--- culti/src/main/java/com/culti/mate/DTO/MatePostDTO.java
+++ culti/src/main/java/com/culti/mate/DTO/MatePostDTO.java
@@ -1,5 +1,43 @@
 package com.culti.mate.DTO;
 
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.culti.mate.enums.MatePostCategory;
+import com.culti.mate.enums.MatePostStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MatePostDTO {
 
+    private Long postId;
+
+    private String title;
+
+    private MatePostCategory category;   
+
+    private LocalDateTime eventAt;
+    private String location;
+    private Integer maxPeople;
+
+    private String description;
+
+    private String writerNickname;
+
+    private MatePostStatus status;   
+    
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+    private LocalDateTime createdAt;
+	
 }
--- culti/src/main/java/com/culti/mate/controller/MateController.java
+++ culti/src/main/java/com/culti/mate/controller/MateController.java
@@ -1,6 +1,8 @@
 package com.culti.mate.controller;
 
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.culti.auth.dto.UserDTO;
+import com.culti.auth.service.UserService;
 import com.culti.mate.entity.MatePost;
 import com.culti.mate.matePage.Criteria;
 import com.culti.mate.matePage.PageDTO;
@@ -23,12 +27,20 @@ import lombok.RequiredArgsConstructor;
 public class MateController {
 	
 	private final MateService mateService; 
+	private final UserService userService;
 
 	@GetMapping("/mate")
 	public String mate(Model model
+			, @AuthenticationPrincipal UserDetails userDetails
 			, @RequestParam(value = "page", defaultValue = "1") int page
 			, @RequestParam(value = "size", defaultValue = "9") int size
 			) {
+		if (userDetails != null) {
+	        String email = userDetails.getUsername();
+	        UserDTO userDTO = userService.findByEmail(email);
+	        model.addAttribute("user", userDTO);
+	    }
+		
 		Page<MatePost> paging = this.mateService.getList(page, size);
 		model.addAttribute("paging", paging);
 		
@@ -38,6 +50,8 @@ public class MateController {
 		model.addAttribute("pageMaker", new PageDTO(criteria, total)  ) ;
 		return "mate/mate";
 	}
+	
+	
 	
 	@PostMapping("/add")
 	public String addPost(@RequestBody String entity) {
