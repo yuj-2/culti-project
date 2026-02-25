305c9cbeaf26445a11b8d3b59e349c843aa51e18
easy3-3 <hanle365711@gmail.com>
2026-02-25 14:52:07 +0900

    찐막 엔티티 정리

--- culti/src/main/java/com/culti/booking/entity/Place.java
+++ culti/src/main/java/com/culti/booking/entity/Place.java
@@ -1,11 +1,18 @@
 package com.culti.booking.entity;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "place")
 public class Place {
     @Id
